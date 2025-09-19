6814395a377dcb9a57a677cf8c720887909d16c4
armando0405 <armandomorenoalvarez5@gnail.com>
2025-09-18 22:33:40 -0500

    [FAMA] feact: creacion dScheduling poniedo dependencia en calse principal

--- src/main/java/com/armando0405/tuboletascraper/TuBoletaScraperApiApplication.java
+++ src/main/java/com/armando0405/tuboletascraper/TuBoletaScraperApiApplication.java
@@ -2,8 +2,10 @@ package com.armando0405.tuboletascraper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TuBoletaScraperApiApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/armando0405/tuboletascraper/dao/repository/ShowInstantaneaRepository.java
+++ src/main/java/com/armando0405/tuboletascraper/dao/repository/ShowInstantaneaRepository.java
@@ -3,7 +3,6 @@ package com.armando0405.tuboletascraper.dao.repository;
 import com.armando0405.tuboletascraper.dao.entity.ShowInstantanea;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
